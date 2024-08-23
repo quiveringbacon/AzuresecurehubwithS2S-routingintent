3304857f0c15ec06062d61904c570902718e5b1b
quiveringbacon <128983862+quiveringbacon@users.noreply.github.com>
2024-08-23 10:52:26 -0500

    Update main.tf
    updated subnets prefixes for new azureRM version

--- main.tf
+++ main.tf
@@ -257,12 +257,12 @@ resource "azurerm_virtual_network" "spoke1-vnet" {
   name                = "spoke1-vnet"
   resource_group_name = azurerm_resource_group.RG.name
   subnet {
-    address_prefix     = "10.150.0.0/24"
+    address_prefixes     = ["10.150.0.0/24"]
     name                 = "default"
     security_group = azurerm_network_security_group.spokevnetNSG.id
   }
   subnet {
-    address_prefix     = "10.150.1.0/24"
+    address_prefixes     = ["10.150.1.0/24"]
     name                 = "GatewaySubnet" 
   }
   timeouts {
@@ -279,12 +279,12 @@ resource "azurerm_virtual_network" "spoke2-vnet" {
   name                = "spoke2-vnet"
   resource_group_name = azurerm_resource_group.RG.name
   subnet {
-    address_prefix     = "10.250.0.0/24"
+    address_prefixes     = ["10.250.0.0/24"]
     name                 = "default"
     security_group = azurerm_network_security_group.spokevnetNSG.id
   }
   subnet {
-    address_prefix     = "10.250.1.0/24"
+    address_prefixes     = ["10.250.1.0/24"]
     name                 = "GatewaySubnet" 
   }
   timeouts {
@@ -302,12 +302,12 @@ resource "azurerm_virtual_network" "onprem-vnet" {
   name                = "onprem-vnet"
   resource_group_name = azurerm_resource_group.RG.name
   subnet {
-    address_prefix     = "192.168.0.0/24"
+    address_prefixes     = ["192.168.0.0/24"]
     name                 = "default"
     security_group = azurerm_network_security_group.spokevnetNSG.id
   }
   subnet {
-    address_prefix     = "192.168.1.0/24"
+    address_prefixes     = ["192.168.1.0/24"]
     name                 = "GatewaySubnet" 
   }
   timeouts {
@@ -609,7 +609,7 @@ resource "azurerm_route_table" "RT" {
   name                          = "to-home"
   location                      = azurerm_resource_group.RG.location
   resource_group_name           = azurerm_resource_group.RG.name
-  disable_bgp_route_propagation = false
+  
 
   route {
     name           = "tohome"
